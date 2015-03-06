cb27f4bc04cc25010b82719cc3d00264db904dbd
Bart Libert <bart.libert@gmail.com>
2015-03-06 11:49:11 +0100

    Modified dbus xml files to work on newever gnome versions

--- dbus.js
+++ dbus.js
@@ -1,125 +1,115 @@
 const Gio = imports.gi.Gio;
 
 const DBusIface =
-    <interface name="org.freedesktop.DBus">
-        <method name="GetNameOwner">
-            <arg type="s" direction="in" />
-            <arg type="s" direction="out" />
-        </method>
+    '<node>' +
+        '<interface name="org.freedesktop.DBus">' +
+        '<method name="GetNameOwner">' +
+            '<arg type="s" direction="in" />' +
+            '<arg type="s" direction="out" />' +
+        '</method>' +
 
-        <method name="ListNames">
-            <arg type="as" direction="out" />
-        </method>
-
-        <signal name="NameOwnerChanged">
-            <arg type="s" direction="out" />
-            <arg type="s" direction="out" />
-            <arg type="s" direction="out" />
-        </signal>
-    </interface>;
+        '<method name="ListNames">' +
+            '<arg type="as" direction="out" />' +
+        '</method>' +
+        '<signal name="NameOwnerChanged">' +
+            '<arg type="s" direction="out" />' +
+            '<arg type="s" direction="out" />' +
+            '<arg type="s" direction="out" />' +
+        '</signal>' +
+    '</interface>' +
+    '</node>';
 const DBusProxy = Gio.DBusProxy.makeProxyWrapper(DBusIface);
 
 const EverpadProviderDBusIface =
-    <interface name="com.everpad.Provider">
-        <method name="list_tags">
-            <arg type="a" direction="out" />
-        </method>
-
-        <method name="list_notebooks">
-            <arg type="a" direction="out" />
-        </method>
-
-        <method name="find_notes">
-            <arg type="s" direction="in" />
-            <arg type="ai" direction="in" />
-            <arg type="ai" direction="in" />
-            <arg type="i" direction="in" />
-            <arg type="i" direction="in" />
-            <arg type="i" direction="in" />
-            <arg type="i" direction="in" />
-            <arg type="a(issxxiassbiaixs)" direction="out" />
-        </method>
-
-        <method name="get_notebook">
-            <arg type="i" direction="in" />
-            <arg type="(isis)" direction="out" />
-        </method>
-
-        <method name="update_note">
-            <arg type="(issxxiassbiaixs)" direction="in" />
-            <arg type="(issxxiassbiaixs)" direction="out" />
-        </method>
-
-        <method name="share_note">
-            <arg type="i" direction="in" />
-        </method>
-
-        <method name="stop_sharing_note">
-            <arg type="i" direction="in" />
-        </method>
-
-        <method name="delete_note">
-            <arg type="i" direction="in" />
-            <arg type="b" direction="out" />
-        </method>
-
-        <method name="get_note_resources">
-            <arg type="i" direction="in" />
-            <arg type="a(issss)" direction="out" />
-        </method>
-
-        <method name="get_status">
-            <arg type="i" direction="out" />
-        </method>
-
-        <method name="get_last_sync">
-            <arg type="s" direction="out" />
-        </method>
-
-        <method name="is_first_synced">
-            <arg type="b" direction="out" />
-        </method>
-
-        <method name="is_authenticated">
-            <arg type="b" direction="out" />
-        </method>
-
-        <method name="sync" />
-
-        <method name="kill" />
-    </interface>;
+    '<node>' +
+        '<interface name="com.everpad.Provider">' +
+            '<method name="list_tags">' +
+                '<arg type="a" direction="out" />' +
+            '</method>' +
+            '<method name="list_notebooks">' +
+                '<arg type="a" direction="out" />' +
+            '</method>' +
+            '<method name="find_notes">' +
+                '<arg type="s" direction="in" />' +
+                '<arg type="ai" direction="in" />' +
+                '<arg type="ai" direction="in" />' +
+                '<arg type="i" direction="in" />' +
+                '<arg type="i" direction="in" />' +
+                '<arg type="i" direction="in" />' +
+                '<arg type="i" direction="in" />' +
+                '<arg type="a(issxxiassbiaixs)" direction="out" />' +
+            '</method>' +
+            '<method name="get_notebook">' +
+                '<arg type="i" direction="in" />' +
+                '<arg type="(isis)" direction="out" />' +
+            '</method>' +
+            '<method name="update_note">' +
+                '<arg type="(issxxiassbiaixs)" direction="in" />' +
+                '<arg type="(issxxiassbiaixs)" direction="out" />' +
+            '</method>' +
+            '<method name="share_note">' +
+                '<arg type="i" direction="in" />' +
+            '</method>' +
+            '<method name="stop_sharing_note">' +
+                '<arg type="i" direction="in" />' +
+            '</method>' +
+            '<method name="delete_note">' +
+                '<arg type="i" direction="in" />' +
+                '<arg type="b" direction="out" />' +
+            '</method>' +
+            '<method name="get_note_resources">' +
+                '<arg type="i" direction="in" />' +
+                '<arg type="a(issss)" direction="out" />' +
+            '</method>' +
+            '<method name="get_status">' +
+                '<arg type="i" direction="out" />' +
+            '</method>' +
+            '<method name="get_last_sync">' +
+                '<arg type="s" direction="out" />' +
+            '</method>' +
+            '<method name="is_first_synced">' +
+                '<arg type="b" direction="out" />' +
+            '</method>' +
+            '<method name="is_authenticated">' +
+                '<arg type="b" direction="out" />' +
+            '</method>' +
+            '<method name="sync" />' +
+            '<method name="kill" />' +
+        '</interface>' +
+    '</node>';
 const EverpadProviderProxy = Gio.DBusProxy.makeProxyWrapper(
     EverpadProviderDBusIface
 );
 
 const EverpadProviderSignalsDBusIface =
-    <interface name="com.everpad.provider">
-        <signal name="sync_state_changed">
-            <arg type="i" direction="out" />
-        </signal>
-
-        <signal name="data_changed" />
-    </interface>;
+    '<node>' +
+        '<interface name="com.everpad.provider">' +
+            '<signal name="sync_state_changed">' +
+                '<arg type="i" direction="out" />' +
+            '</signal>' +
+            '<signal name="data_changed" />' +
+        '</interface>' +
+    '</node>';
 const EverpadProviderSignalsProxy = Gio.DBusProxy.makeProxyWrapper(
     EverpadProviderSignalsDBusIface
 );
 
 const EverpadAppDBusIface =
-    <interface name="com.everpad.App">
-        <method name="all_notes" />
-        <method name="kill" />
-        <method name="settings" />
-        <method name="create" />
-
-        <method name="open">
-            <arg type="i" direction="in" />
-        </method>
-
-        <method name="open_with_search_term">
-            <arg type="i" direction="in" />
-            <arg type="s" direction="in" />
-        </method>
-    </interface>;
+    '<node>' +
+        '<interface name="com.everpad.App">' +
+            '<method name="all_notes" />' +
+            '<method name="kill" />' +
+            '<method name="settings" />' +
+            '<method name="create" />' +
+            '<method name="open">' +
+                '<arg type="i" direction="in" />' +
+            '</method>' +
+            '<method name="open_with_search_term">' +
+                '<arg type="i" direction="in" />' +
+                '<arg type="s" direction="in" />' +
+            '</method>' +
+        '</interface>' +
+    '</node>';
 const EverpadAppProxy = Gio.DBusProxy.makeProxyWrapper(
     EverpadAppDBusIface
 );
